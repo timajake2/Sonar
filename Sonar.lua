6e8075d41ab20a410bde8d0a60db8c3941f89746
timajake2 <urvancevat85@gmail.com>
2026-07-25 20:49:59 +0900

    Update Sonar.lua

--- Sonar.lua
+++ Sonar.lua
@@ -233,7 +233,6 @@ KeybindsButton.Parent = MainFrame
 local KeybindsCorner = Instance.new("UICorner")
 KeybindsCorner.CornerRadius = UDim.new(0, 6)
 KeybindsCorner.Parent = KeybindsButton
-
 -- ================= ЛОГИКА И ИНТЕРАКТИВНОСТЬ =================
 
 local silentAimFOV = 100 -- Скрытый радиус захвата вокруг курсора (в пикселях)
